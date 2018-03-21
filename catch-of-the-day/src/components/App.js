ddd0ae8fd4b1c91504fbf65ec47c60a3a57ab273
Roman <rromanishin@ucell.com.ua>
2018-03-21 15:09:18 +0200

    start lesson 20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,10 @@ class App extends React.Component{
     };
     componentDidMount(){
         const params = this.props.match.params;
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if(localStorageRef){
+            this.setState({order: JSON.parse(localStorageRef)});
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`,{
             context: this,
             state: 'fishes'
@@ -23,6 +27,7 @@ class App extends React.Component{
     }
     componentDidUpdate(){
         console.log('Update');
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
     }
 
     addFish = fish => {
