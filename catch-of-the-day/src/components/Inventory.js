48d3199e411bde5ee4241b97f371124dcf5ed159
Roman <rromanishin@ucell.com.ua>
2018-03-22 14:23:38 +0200

    start lesson 25

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import firebase from 'firebase';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import base, {firebaseApp} from '../base';
 
 class Inventory extends React.Component{
+
+    state = {
+        uid: null,
+        owner: null
+    }
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user=> {
+                if (user) {
+                    this.authHandler({user})
+                }
+            }
+        )
+    }
+    authHandler = async authData =>{
+        const store = await base.fetch(this.props.storeId, {context: this});
+        if(!store.owner){
+            await base.post(`${this.props.storeId}/owner`,{
+                data: authData.user.uid
+            })
+        }
+        this.setState({
+            uid: authData.user.uid,
+            owner: store.owner || authData.user.uid
+        })
+    }
+    authenticate = (provider) =>{
+        const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+        firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+    }
+    logOut = async () =>{
+        await  firebase.auth().signOut();
+        this.setState({uid:null});
+    }
     render(){
+        const logOut = <button onClick={this.logOut}>Logout</button>
+        if (!this.state.uid) {
+            {logOut}
+            return <Login authenticate={this.authenticate}/>;
+        }
         return (
             <div className="inventory">
                 <h2>Inventory!</h2>
+                {logOut}
                 {Object.keys(this.props.fishes).map(key =>
                     <EditFishForm
                         key = {key}
